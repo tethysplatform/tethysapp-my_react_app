59f8fc2ad8368909ada65a96589eab973c4c3529
Nathan Swain <nswain@aquaveo.com>
2022-06-14 13:24:32 -0600

    Add support for development and production .env configs

--- reactapp/config/webpack.config.js
+++ reactapp/config/webpack.config.js
@@ -2,79 +2,84 @@ const path = require('path');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-	entry: ['./reactapp'],
-	output: {
-		path: path.resolve(__dirname, '../../tethysapp/my_react_app/public/frontend'),
-		filename: '[name].js',
-		publicPath: '/static/my_react_app/frontend/',
-	},
-	plugins: [
-		new Dotenv({
-			path: './reactapp/config/.env'
-		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'babel-loader',
-					},
-				],
-			},
-			{
-				test: /\.css$/,
-				// exclude: /node_modules/,
-				use: [
-					{
-						loader: 'style-loader',
-					},
-					{
-						loader: 'css-loader',
-					},
-				],
-			},
-			{
-				test: /\.(scss|sass)$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'style-loader',
-					},
-					{
-						loader: 'css-loader',
-					},
-					{
-						loader: 'sass-loader',
-					},
-				],
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg|mp4|mp3)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							outputPath: '',
-						},
-					},
-				],
-			},
-		],
-	},
-	optimization: {
-		minimize: true,
-	},
-	devServer: {
-		proxy: {
-			'!/static/my_react_app/frontend/**': {
-				target: 'http://localhost:8000', // points to django dev server
-				changeOrigin: true,
-			},
+module.exports = (env, argv) => {
+	const dotEnvPath = `./reactapp/config/${argv.mode}.env`;
+	console.log(`Building in ${argv.mode} mode...`);
+	console.log(`=> Using .env config at "${dotEnvPath}"`);
+	return {
+		entry: ['./reactapp'],
+		output: {
+			path: path.resolve(__dirname, '../../tethysapp/my_react_app/public/frontend'),
+			filename: '[name].js',
+			publicPath: '/static/my_react_app/frontend/',
 		},
-		open: true,
-	},
+		plugins: [
+			new Dotenv({
+				path: dotEnvPath
+			}),
+		],
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'babel-loader',
+						},
+					],
+				},
+				{
+					test: /\.css$/,
+					// exclude: /node_modules/,
+					use: [
+						{
+							loader: 'style-loader',
+						},
+						{
+							loader: 'css-loader',
+						},
+					],
+				},
+				{
+					test: /\.(scss|sass)$/,
+					exclude: /node_modules/,
+					use: [
+						{
+							loader: 'style-loader',
+						},
+						{
+							loader: 'css-loader',
+						},
+						{
+							loader: 'sass-loader',
+						},
+					],
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg|mp4|mp3)$/,
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								outputPath: '',
+							},
+						},
+					],
+				},
+			],
+		},
+		optimization: {
+			minimize: true,
+		},
+		devServer: {
+			proxy: {
+				'!/static/my_react_app/frontend/**': {
+					target: 'http://localhost:8000', // points to django dev server
+					changeOrigin: true,
+				},
+			},
+			open: true,
+		},
+	}
 };

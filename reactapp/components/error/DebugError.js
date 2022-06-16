97bab114027f4693a4a0f715c67ede91a0cacfa3
Nathan Swain <nswain@aquaveo.com>
2022-06-16 11:38:18 -0600

    Restyle the debug error page

--- reactapp/components/error/DebugError.js
+++ reactapp/components/error/DebugError.js
@@ -1,4 +1,5 @@
 import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
 import Prism from 'prismjs';
 import PropTypes from 'prop-types';
 import { useEffect } from 'react';
@@ -30,56 +31,63 @@ const DebugError = ({ error, errorInfo }) => {
 
   return (
     <div className="d-flex flex-column h-100 bg-light">
+      
       <main className="flex-shrink-0">
-        <div className="px-4 py-2 border-bottom" style={{ backgroundColor: "#fff3cd" }}>
-          <h1 className="text-secondary">{error && error.toString()}</h1>
-        </div>
-        <div className="px-4 py-2" >
-          <h4>Component Stack</h4>
-          <pre className="rounded">
-            <code className={`language-${languageName}`}>
-              {errorInfo.componentStack.replace('\n', '')}
-            </code>
-          </pre>
-        </div>
-        <div className="px-4 py-2" >
-          <Alert variant="info">
-            <Alert.Heading>Tip</Alert.Heading>
-            <p>Use the <b>React Developer Tools</b> extension for your browser to debug this error:</p>
-            <ul>
-              <li>
-                <a 
-                  href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi" 
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  React Developer Tools Chrome Extension
-                </a>
-              </li>
-              <li>
-                <a 
-                  href="https://addons.mozilla.org/en-US/firefox/addon/react-devtools/" 
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  React Developer Tools Firefox Add-on
-                </a>
-              </li>
-              <li>
-                <a 
-                  href="https://microsoftedge.microsoft.com/addons/detail/react-developer-tools/gpphkfbcpidddadnkolkpfckpihlkkil" 
-                  target="_blank" 
-                  rel="noopener noreferrer"
-                >
-                  React Developer Tools Microsoft Edge Add-on
-                </a>
-              </li>
-            </ul>
-          </Alert>
-        </div>
+        <Container>
+          <div id="error-message-wrapper" className="my-4">
+            <Alert variant="danger">
+              <h1>{error && error.toString()}</h1>
+            </Alert>
+          </div>
+          <div id="component-stack-wrapper" className="mb-4">
+            <h4 className="mb-3">Component Stack</h4>
+            <pre className="rounded">
+              <code className={`language-${languageName}`}>
+                {errorInfo.componentStack.replace('\n', '')}
+              </code>
+            </pre>
+          </div>
+          <div id="tip-wrapper" className="mb-4">
+            <Alert variant="info" className="d-inline-block">
+              <Alert.Heading>Tip</Alert.Heading>
+              <p>Use the <b>React Developer Tools</b> extension for your browser to debug this error:</p>
+              <ul>
+                <li>
+                  <a 
+                    href="https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    React Developer Tools Chrome Extension
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://addons.mozilla.org/en-US/firefox/addon/react-devtools/" 
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    React Developer Tools Firefox Add-on
+                  </a>
+                </li>
+                <li>
+                  <a 
+                    href="https://microsoftedge.microsoft.com/addons/detail/react-developer-tools/gpphkfbcpidddadnkolkpfckpihlkkil" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                  >
+                    React Developer Tools Microsoft Edge Add-on
+                  </a>
+                </li>
+              </ul>
+            </Alert>
+          </div>
+        </Container>
       </main>
       <footer className="mt-auto">
-        <Alert variant="secondary" className="mb-0"><b>Important!</b> You&apos;re seeing this error because you have <code>TETHYS_DEBUG = true</code> in your <code>.env</code> file. Change that to <code>false</code> to display the standard error message page.</Alert>
+        <Alert variant="warning" className="mb-0 rounded-0 px-5">
+          <b>Important!</b> You&apos;re seeing this error because you have <code>TETHYS_DEBUG = true</code> in your <code>.env</code> file. Change that to <code>false</code> to display the standard error message page.
+        </Alert>
       </footer>
     </div>
   );

aa685a90702f22b0831f963d32b06006188522af
Nathan Swain <nswain@aquaveo.com>
2022-06-17 16:03:32 -0600

    Make header more responsive for small screens

--- reactapp/components/layout/Header.js
+++ reactapp/components/layout/Header.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,17 +11,21 @@ import HeaderButton from 'components/buttons/HeaderButton';
 import NavButton from 'components/buttons/NavButton';
 import { AppContext } from 'components/context';
 
+const CustomNavBar = styled(Navbar)`
+  min-height: var(--ts-header-height);
+`;
+
 const Header = ({onNavChange}) => {
   const {tethysApp, user} = useContext(AppContext);
   const showNav = () => onNavChange(true);
 
   return (
     <>
-        <Navbar fixed="top" expand="lg" bg="primary" variant="dark" className="shadow">
-          <Container fluid className="px-4">
-            <NavButton onClick={showNav} className="me-2"></NavButton>
+        <CustomNavBar fixed="top" bg="primary" variant="dark" className="shadow">
+          <Container as="header" fluid className="px-4">
+            <NavButton onClick={showNav}></NavButton>
             <LinkContainer to="/">
-              <Navbar.Brand>
+              <Navbar.Brand className="mx-0 d-none d-sm-block">
                 <img 
                   src={tethysApp.icon} 
                   width="30" 
@@ -37,7 +42,7 @@ const Header = ({onNavChange}) => {
               <HeaderButton href={tethysApp.exitUrl} tooltipPlacement="bottom" tooltipText="Exit"><BsX size="1.5rem"/></HeaderButton>
             </Form>
           </Container>
-        </Navbar>
+        </CustomNavBar>
     </>
   );
 };

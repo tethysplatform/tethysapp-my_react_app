1be8c8c1cddf18e854f21db41fdb48803ff253df
Nathan Swain <nswain@aquaveo.com>
2022-06-21 10:43:30 -0600

    Clean up

--- tethysapp/my_react_app/controllers.py
+++ tethysapp/my_react_app/controllers.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-import plotly.graph_objects as go
-import plotly.io as pio
 import pandas as pd
 
 from tethys_sdk.routing import controller
